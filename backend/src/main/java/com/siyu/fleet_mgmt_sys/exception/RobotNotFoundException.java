f44f4becde4ba486dcf891fa7dcdd6959edd26f2
IterativeImprovement <siyuchen03@gmail.com>
2026-05-30 18:45:45 +0800

    Fixed error with delete robot. Amended exception files so that frontend can receive proper NotFound messages.

--- backend/src/main/java/com/siyu/fleet_mgmt_sys/exception/RobotNotFoundException.java
+++ backend/src/main/java/com/siyu/fleet_mgmt_sys/exception/RobotNotFoundException.java
@@ -1,6 +1,6 @@
 package com.siyu.fleet_mgmt_sys.exception;
 
-public class RobotNotFoundException extends RuntimeException {
+public class RobotNotFoundException extends NotFoundException {
     public RobotNotFoundException(Long id) {
         super("Robot with id " + id + " not found");
     }
